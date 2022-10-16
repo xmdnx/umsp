017ed3e522abffe9bc66835bc31840fb479401f0
xmdnx <smcartix@gmail.com>
2022-10-16 20:28:52 +0300

    update

--- web/homepage/models.py
+++ web/homepage/models.py
@@ -1,3 +1,8 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class Track(models.Model):
+    title = models.CharField('Название песни', max_length=100)
+    author = models.CharField('Исполнитель', max_length=100)
+    duration = models.CharField('Длительность', max_length=10)
+    file = models.FieldFile('Файл (mp3)')
